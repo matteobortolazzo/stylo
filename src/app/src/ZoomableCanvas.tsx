ae74e3d8cc455c1deda32e12d8d7d72eba2b4ecf
Matteo Bortolazzo <matteobortolazzo@pm.me>
2023-03-21 22:30:24 +0100

    Cursor zoom

--- src/app/src/ZoomableCanvas.tsx
+++ src/app/src/ZoomableCanvas.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useState } from "react";
+import React, { FC, useState, useRef } from "react";
 import DOMPurify from "dompurify";
 
 type ZoomableCanvasProps = {
@@ -6,12 +6,20 @@ type ZoomableCanvasProps = {
 };
 
 const ZoomableCanvas: FC<ZoomableCanvasProps> = ({ html: source }) => {
+  const canvasRef = useRef<HTMLDivElement>(null);
   const [zoom, setZoom] = useState(1);
   const [canvasPosition, setCanvasPosition] = useState({ x: 0, y: 0 });
 
   function handleZoom(event: any) {
-    const delta = event.deltaY > 0 ? -0.1 : 0.1;
-    setZoom(Math.min(Math.max(0.1, zoom + delta), 3));
+    const cursorPosition = getCursorPosition(event);
+    const zoomFactor = event.deltaY > 0 ? 0.9 : 1.1;
+    const newZoom = Math.min(Math.max(0.1, zoom * zoomFactor), 3);
+    const zoomDelta = newZoom - zoom;
+    setZoom(newZoom);
+    setCanvasPosition({
+      x: canvasPosition.x + cursorPosition.x * zoomDelta,
+      y: canvasPosition.y + cursorPosition.y * zoomDelta,
+    });
   }
 
   function handlePanStart(event: any) {
@@ -38,11 +46,20 @@ const ZoomableCanvas: FC<ZoomableCanvasProps> = ({ html: source }) => {
     document.addEventListener("mouseup", handlePanEnd);
   }
 
+  function getCursorPosition(event: any) {
+    const canvas = canvasRef.current!;
+    const canvasRect = canvas.getBoundingClientRect();
+    const cursorX = (event.clientX - canvasRect.left) / zoom;
+    const cursorY = (event.clientY - canvasRect.top) / zoom;
+    return { x: cursorX, y: cursorY };
+  }
+
   const sanitizedHtml = DOMPurify.sanitize(source);
   const scaledHtml = `<div style="transform: scale(${zoom}) translate(${canvasPosition.x}px, ${canvasPosition.y}px) translateZ(0); position: absolute; left: 0; top: 0;">${sanitizedHtml}</div>`;
 
   return (
     <div
+      ref={canvasRef}
       style={{
         position: "relative",
         overflow: "hidden",
